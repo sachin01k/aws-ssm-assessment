460d3d45dfda2a93ff050f358418e1691ce09862
sachin01k <sachinkamble825@gmail.com>
2026-01-11 17:10:46 +0530

    feature assessment

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,17 +1,37 @@
 import boto3
 import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ssm = boto3.client("ssm")
 
 def lambda_handler(event, context):
-    ssm = boto3.client("ssm")
+    try:
+        parameter_name = os.environ.get("SSM_PARAMETER_NAME")
 
-    parameter_name = os.environ.get("SSM_PARAMETER_NAME")
+        if not parameter_name:
+            raise ValueError("SSM_PARAMETER_NAME environment variable is not set")
 
-    response = ssm.get_parameter(
-        Name=parameter_name,
-        WithDecryption=False
-    )
+        logger.info(f"Fetching SSM parameter: {parameter_name}")
 
-    return {
-        "statusCode": 200,
-        "value": response["Parameter"]["Value"]
-    }
+        response = ssm.get_parameter(
+            Name=parameter_name,
+            WithDecryption=True
+        )
+
+        value = response["Parameter"]["Value"]
+
+        return {
+            "statusCode": 200,
+            "value": value
+        }
+
+    except Exception as e:
+        logger.error("Error while fetching SSM parameter", exc_info=True)
+
+        return {
+            "statusCode": 500,
+            "error": str(e)
+        }
